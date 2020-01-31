538d94b6b4e5e505e3c815f6e47600e292bcfb90
Spencer Smith <linkhylian8@gmail.com>
2020-01-30 20:10:02 -0400

    added labels to panel

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "sys", "net", "snd", "img", "ex1", "ex2", "ex3", "ex4", "ex5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
